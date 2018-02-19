6d15195e30a096aa9bd61a1b45b33e7b4679a7b1
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-19 14:20:30 +0100

    Clear button

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Form, FormControl, Button } from 'react-bootstrap';
 import Note from './Note';
+import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
+
+const cookie_key = 'NOTES';
 
 class App extends Component {
     constructor() {
@@ -12,12 +15,24 @@ class App extends Component {
         }
     }
 
+    componentDidMount() {
+        this.setState({ notes: read_cookie(cookie_key) });
+    }
+
     submit() {
         const { notes, text } = this.state;
 
         notes.push({ text });
 
         this.setState({ notes })
+
+        bake_cookie(cookie_key, this.state.notes);
+    }
+
+    clear() {
+        delete_cookie(cookie_key);
+
+        this.setState({ notes: [] });
     }
 
     render() {
@@ -36,6 +51,8 @@ class App extends Component {
                         )
                     })
                 }
+                <hr />
+                <Button onClick={() => this.clear()}>Clear Notes</Button>
             </div>
         )
     }
--- src/components/Note.js
+++ src/components/Note.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Note extends Component {
     render() {
         return (
-            <div className="note">
+            <div className='note'>
                 <p>{ this.props.note.text }</p>
             </div>
         )
