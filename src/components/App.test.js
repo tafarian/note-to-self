93b4bb930768a8605d0bfcfee78523a63f1ee2bb
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-20 14:32:21 +0100

    Testing main component

---
+++ src/components/App.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+    let app = mount(<App />);
+
+    it('renders the App title', () => {
+        // console.log(app.debug())
+        expect(app.find('h2').text()).toEqual('Note to Self');
+    });
+
+    it('renders the clear button', () => {
+        expect(app.find('.btn').at(1).text()).toEqual('Clear Notes');
+    });
+
+    describe('when rendering the form', () => {
+        it('creates a Form component', () => {
+            expect(app.find('Form').exists()).toBe(true);
+        });
+        it('renders a FormControl component', () => {
+            expect(app.find('FormControl').exists()).toBe(true);
+        });
+        it('renders a submit button', () => {
+            expect(app.find('.btn').at(0).text()).toEqual('Submit');
+        });
+
+    });
+});
