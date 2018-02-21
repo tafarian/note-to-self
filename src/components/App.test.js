73bea403f278c84d5dff5d9168dff9e578cdf7bf
Jakub Wyczesany <ghaelios@gmail.com>
2018-02-21 14:13:37 +0100

    Testing cookies

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -39,14 +39,32 @@ describe('App', () => {
             expect(app.state().text).toEqual(testNote);
         });
 
-        describe('and subimtting the new note', () => {
+        describe('and submitting the new note', () => {
             beforeEach(() => {
                 app.find('.btn').at(0).simulate('click');
+            });
+
+            afterEach(() => {
+                app.find('.btn').at(1).simulate('click');
             });
 
             it('adds the new note to state', () => {
                 // console.log(app.state())
                 expect(app.state().notes[0].text).toEqual(testNote)
+            });
+
+            describe('and remounting the component', () => {
+                let app2;
+
+                beforeEach(() => {
+                    app2 = mount(<App />)
+                });
+
+                it('reads the stored note cookies', () => {
+                    // console.log(app2.state());
+                    expect(app2.state().notes).toEqual([{ text: testNote }])
+                })
+
             });
 
             describe('and clicking the clear button', () => {
